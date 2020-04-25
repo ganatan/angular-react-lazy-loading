7c1961474fdf3b293348eab1a8f443ad2780e2d3
ganatan <dannyganatan@gmail.com>
2020-04-25 16:07:31 +0200

    Add JsonPipe and routing

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HomeComponent } from './modules/general/home/home.component';
-import { NotFoundComponent } from './modules/general/not-found/not-found.component';
+/* import { HomeComponent } from './modules/general/home/home.component';
+import { NotFoundComponent } from './modules/general/not-found/not-found.component'; */
 import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    NotFoundComponent,
+/*    HomeComponent,
+    NotFoundComponent, */
   ],
   imports: [
     BrowserModule,
