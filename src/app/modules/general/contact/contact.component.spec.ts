e9061f2a6df6deb9470678e81f1460bc0eb8e8e0
ganatan <dannyganatan@gmail.com>
2021-12-14 18:54:46 +0100

    New doc Version

--- src/app/modules/general/contact/contact.component.spec.ts
+++ src/app/modules/general/contact/contact.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { RouterTestingModule } from '@angular/router/testing';
 
 import { ContactComponent } from './contact.component';
 
@@ -8,9 +9,11 @@ describe('ContactComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ ContactComponent ]
-    })
-    .compileComponents();
+      imports: [
+        RouterTestingModule
+      ],
+      declarations: [ContactComponent]
+    }).compileComponents();
   });
 
   beforeEach(() => {
