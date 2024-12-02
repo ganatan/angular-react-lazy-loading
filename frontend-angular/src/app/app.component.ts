0cecea769ee8a9e3ea9a23a555677b314102e60e
ganatan <dannyganatan@gmail.com>
2024-12-02 14:10:24 +0100

    Update Angular 19.0.1

--- frontend-angular/src/app/app.component.ts
+++ frontend-angular/src/app/app.component.ts
@@ -4,10 +4,9 @@ import { RouterLink, RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  standalone: true,
   imports: [CommonModule, RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrl: './app.component.css'
 })
 export class AppComponent {
   title = 'angular-routing';
