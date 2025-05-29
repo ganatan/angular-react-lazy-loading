cc9b7acc4cdf1c3e009df15592b6fa4cb478b3c1
ganatan <dannyganatan@gmail.com>
2025-05-29 10:43:04 +0200

    Add features with angular 20

--- frontend-angular/src/app/app.routes.ts
+++ frontend-angular/src/app/app.routes.ts
@@ -1,41 +1,32 @@
 import { Routes } from '@angular/router';
 
 import { Home } from './features/home/home';
-import { Login } from './features/login/login';
-import { Signup } from './features/signup/signup';
 import { NotFound } from './features/not-found/not-found';
-import { About } from './features/about/about';
-import { Contact } from './features/contact/contact';
-
-import { Experience } from './features/about/experience/experience';
-import { Skill } from './features/about/skill/skill';
-
-import { Mailing } from './features/contact/mailing/mailing';
-import { Mapping } from './features/contact/mapping/mapping';
-import { Website } from './features/contact/website/website';
 
 export const routes: Routes = [
   { path: '', component: Home, },
 
-  { path: 'login', component: Login },
-  { path: 'signup', component: Signup },
-
   {
-    path: 'about', component: About,
-    children: [
-      { path: '', component: Experience },
-      { path: 'experience', component: Experience },
-      { path: 'skill', component: Skill },
-    ],
+    path: 'login',
+    loadComponent: () => import(`./features/login/login`)
+      .then(mod => mod.Login)
   },
   {
-    path: 'contact', component: Contact,
-    children: [
-      { path: '', component: Mailing },
-      { path: 'mailing', component: Mailing },
-      { path: 'mapping', component: Mapping },
-      { path: 'website', component: Website },
-    ],
+    path: 'signup',
+    loadComponent: () => import(`./features/signup/signup`)
+      .then(mod => mod.Signup)
+  },
+  
+  {
+    path: 'contact',
+    loadChildren: () => import(`./features/contact/contact.routes`)
+      .then(routes => routes.routes)
+  },
+
+  {
+    path: 'about',
+    loadChildren: () => import('./features/about/about.routes')
+    .then(routes => routes.routes)
   },
 
   { path: '**', component: NotFound }
